c609b4c0b9b1bfe90ce0ff4c543ef6d5ddc2e9c8
mochenel <freddy980404@gmail.com>
2020-10-30 12:08:19 +0200

    fix icons

---
+++ js/sendMail.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() { 
+// 	$('#send').click(sendMail);
+// 	function sendMail() { 
+
+//         let person = {
+//             name: $("#name").val(),
+//             email:$("#email").val(),
+//             tel:$("#tel").val(),
+//             message:$("#message").val()
+//         }
+
+//         $('#target').html('sending..');
+
+//         return $.ajax({
+//             url: 'http://localhost:3000/send',
+//             type: 'post',
+//             dataType: 'json',
+//             async: false,
+//             contentType: 'application/json',
+//             success: function (data) {
+//             	console.log("****** "+data)
+//                 $('#target').html(data);
+//             },
+//             data: JSON.stringify(person)
+//         });
+	
+// } 
+})
